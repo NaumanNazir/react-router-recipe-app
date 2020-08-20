1987bb29c4354d3949251d41d0988a60a100d7da
Nauman Nazir <nauman001@yahoo.com>
2020-08-20 23:41:41 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Form from './components/Form'
+import Recipes from './components/Recipes'
 
 import './App.css';
 
@@ -7,12 +8,21 @@ const APP_ID = "e8aa04e7"
 const API_KEY = "ffa69918f480729b56adb7b4fa299969"
 
 class App extends Component {
+  state = {
+    recipes: []
+  }
+
   getRecipe = async (e) => {
     const recipeName = e.target.elements.recipeName.value
     e.preventDefault()
-    const api_call = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`)
+    const api_call = await fetch(`https://api.edamam.com/search?q=${recipeName}&app_id=${APP_ID}&app_key=${API_KEY}`)
     const data = await api_call.json()
-    console.log(data.hits[0].recipe.label)
+
+    this.setState({
+      recipes: data.hits
+    })
+
+    console.log(this.state.recipes)
   }
 
   render() {
@@ -20,8 +30,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title"> Recipe Search </h1>
-          <Form getRecipe={this.getRecipe} />
         </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes} />
       </div>
     );
   }
